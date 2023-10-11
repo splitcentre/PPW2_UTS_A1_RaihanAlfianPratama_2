0de239d9a158f805931555c30acfad662d242754
splitcentre <raihanalfianpratama@gmail.com>
2023-10-11 09:31:44 +0700

    routes

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-Route::resource('products', ProductController::class);
+Route::resource('products', 'ProductController');
